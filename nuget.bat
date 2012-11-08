38c1bda172cdee087eced2e8bf136de263a1aa2a
Adam Schroder <adamschroder@gmail.com>
2012-11-08 15:21:50 +1100

    Bump version to 0.6.2

---
+++ nuget.bat
@@ -0,0 +1,2 @@
+.\.nuget\NuGet.exe pack SchoStack.Web\SchoStack.Web.csproj -Prop Configuration=Release -OutputDirectory dev_nuget
+.\.nuget\NuGet.exe pack SchoStack.Web.FluentValidation\SchoStack.Web.FluentValidation.csproj -Prop Configuration=Release -OutputDirectory dev_nuget
